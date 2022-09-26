c22bc5998a27eec46bfe81d935592589b88a94ad
SaluRamos <saluc.ramos@outlook.com>
2022-09-25 23:15:29 -0300

    solve gitignore

---
+++ scripts/Smoothie.java
@@ -0,0 +1,30 @@
+package scripts;
+import java.util.TreeSet;
+
+public class Smoothie{
+
+    private String name;
+    private TreeSet<String> ingredients;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public TreeSet<String> getIngredients(){
+        return this.ingredients;
+    }
+
+    public void setName(String name){
+        this.name = name.toLowerCase();
+    }
+
+    public void setIngredients(String[] ingredients){
+        TreeSet<String> ingredientsTree = new TreeSet<String>();
+        for(int i = 0; i < ingredients.length; i++){
+            ingredients[i] = ingredients[i].toLowerCase();
+            ingredientsTree.add(ingredients[i]);
+        }
+        this.ingredients = ingredientsTree;
+    }
+
+}
